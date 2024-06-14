6ae3b8a06c50abfbee7378bf7d495a20c4e79987
Christina Sørensen <christina@cafkafk.com>
2024-06-14 06:35:39 +0200

    feat: move to crane, deny.toml, a lot of stuff, wow I can code sometimes
    Signed-off-by: Christina Sørensen <christina@cafkafk.com>

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
 
     systems.url = "github:nix-systems/default";
 
@@ -29,8 +29,20 @@
       };
     };
 
-    naersk = {
-      url = "github:nix-community/naersk";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-analyzer-src.follows = "";
+    };
+
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
     };
 
     treefmt-nix = {
@@ -61,11 +73,13 @@
   outputs = {
     self,
     flake-utils,
-    naersk,
     nixpkgs,
     treefmt-nix,
     rust-overlay,
     pre-commit-hooks,
+    crane,
+    fenix,
+    advisory-db,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (
@@ -76,34 +90,54 @@
           inherit system overlays;
         };
 
+        inherit (pkgs) lib;
+
         toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
 
-        naersk' = pkgs.callPackage naersk {
-          cargo = toolchain;
-          rustc = toolchain;
-          clippy = toolchain;
-        };
-
-        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+        craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+        src = craneLib.cleanCargoSource (craneLib.path ./.);
 
         darwinBuildInputs = with pkgs; with darwin.apple_sdk.frameworks; [libiconv Security SystemConfiguration];
 
-        buildInputs = with pkgs; [pkg-config openssl] ++ lib.optionals stdenv.isDarwin darwinBuildInputs;
-      in rec {
-        # For `nix fmt`
-        formatter = treefmtEval.config.build.wrapper;
+        # Common arguments can be set here to avoid repeating them later
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
 
-        packages = {
-          # For `nix build` & `nix run`:
-          default = naersk'.buildPackage {
-            src = ./.;
-            doCheck = true; # run `cargo test` on build
-            copyBins = true;
-            copyLibs = true;
-            singleStep = true;
-            inherit buildInputs;
+          buildInputs = with pkgs;
+            [
+              # Add additional build inputs here
+              openssl
+            ]
+            ++ lib.optionals pkgs.stdenv.isDarwin darwinBuildInputs;
 
-            nativeBuildInputs = with pkgs; [makeWrapper installShellFiles];
+          # Additional environment variables can be set directly
+          # MY_CUSTOM_VAR = "some value";
+
+          nativeBuildInputs = with pkgs; [
+            makeWrapper
+            installShellFiles
+            pkg-config
+          ];
+        };
+
+        craneLibLLvmTools =
+          craneLib.overrideToolchain
+          (fenix.packages.${system}.complete.withComponents [
+            "cargo"
+            "llvm-tools"
+            "rustc"
+          ]);
+
+        # Build *just* the cargo dependencies, so we can reuse
+        # all of that work (e.g. via cachix) when running in CI
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        # Build the actual crate itself, reusing the dependency
+        # artifacts from above.
+        ha-registry = craneLib.buildPackage (commonArgs
+          // {
+            inherit cargoArtifacts;
 
             MAN_OUT = "./man";
 
@@ -121,73 +155,76 @@
             '';
 
             meta.mainProgram = "ha-registry";
-          };
+          });
 
-          container = pkgs.dockerTools.buildLayeredImage {
-            name = "ha-tregistry";
-            tag = "latest";
-            contents = [packages.default pkgs.cacert];
-            config = {
-              Labels = {
-                "org.opencontainers.image.source" = "https://github.com/cafkafk/ha-registry";
-                "org.opencontainers.image.description" = "ha-registry: High Availability Container Registry";
-                "org.opencontainers.image.license" = "AGPL-3.0-only";
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+      in rec {
+        # For `nix fmt`
+        formatter = treefmtEval.config.build.wrapper;
+
+        packages =
+          {
+            default = ha-registry;
+
+            container = pkgs.dockerTools.buildLayeredImage {
+              name = "ha-tregistry";
+              tag = "latest";
+              contents = [packages.default pkgs.cacert];
+              config = {
+                Labels = {
+                  "org.opencontainers.image.source" = "https://github.com/cafkafk/ha-registry";
+                  "org.opencontainers.image.description" = "ha-registry: High Availability Container Registry";
+                  "org.opencontainers.image.license" = "AGPL-3.0-only";
+                };
+                Env = [
+                  "RUST_LOG=trace"
+                ];
+                Cmd = ["/bin/ha-registry"];
               };
-              Env = [
-                "RUST_LOG=trace"
-              ];
-              Cmd = ["/bin/ha-registry"];
-            };
-          };
-
-          # Run `nix build .#check` to check code
-          check = naersk'.buildPackage {
-            src = ./.;
-            mode = "check";
-            inherit buildInputs;
-          };
-
-          # Run `nix build .#test` to run tests
-          test = naersk'.buildPackage {
-            src = ./.;
-            mode = "test";
-            inherit buildInputs;
-          };
-
-          # Run `nix build .#clippy` to lint code
-          clippy = naersk'.buildPackage {
-            src = ./.;
-            mode = "clippy";
-            inherit buildInputs;
-          };
-
-          distribution-spec = pkgs.buildGoModule rec {
-            name = "distribution-spec";
-            version = "1.1.0";
-
-            src = pkgs.fetchFromGitHub {
-              owner = "opencontainers";
-              repo = "distribution-spec";
-              rev = "v${version}";
-              hash = "sha256-GL28YUwDRicxS65E7SDR/Q3tJOWN4iwgq4AGBjwVPzA=";
             };
 
-            buildPhase = ''
-              go test -c -o conformance.test
-            '';
+            distribution-spec = pkgs.buildGoModule rec {
+              name = "distribution-spec";
+              version = "1.1.0";
 
-            installPhase = ''
-              mkdir -p $out/bin
-              cp conformance.test $out/bin
-            '';
+              src = pkgs.fetchFromGitHub {
+                owner = "opencontainers";
+                repo = "distribution-spec";
+                rev = "v${version}";
+                hash = "sha256-GL28YUwDRicxS65E7SDR/Q3tJOWN4iwgq4AGBjwVPzA=";
+              };
 
-            vendorHash = "sha256-5gn9RpjCALZB/GFjlJHDqPs2fIHl7NJr5QjPmsLnnO4=";
-            modRoot = "conformance";
+              buildPhase = ''
+                go test -c -o conformance.test
+              '';
+
+              installPhase = ''
+                mkdir -p $out/bin
+                cp conformance.test $out/bin
+              '';
+
+              vendorHash = "sha256-5gn9RpjCALZB/GFjlJHDqPs2fIHl7NJr5QjPmsLnnO4=";
+              modRoot = "conformance";
+            };
+          }
+          // lib.optionalAttrs (!pkgs.stdenv.isDarwin) {
+            serde-yaml-llvm-coverage = craneLibLLvmTools.cargoLlvmCov (commonArgs
+              // {
+                inherit cargoArtifacts;
+              });
           };
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = ha-registry;
         };
 
         # For `nix develop`:
-        devShells.default = pkgs.mkShell {
+        devShells.default = craneLib.devShell {
+          # Inherit inputs from checks.
+          checks = self.checks.${system};
+
+          # Additional dev-shell environment variables can be set directly
+          # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
           buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
           nativeBuildInputs = with pkgs; [rustup toolchain just zip reuse pkg-config openssl vhs fish statix clippy];
           inherit (self.checks.${system}.pre-commit-check) shellHook;
@@ -195,7 +232,53 @@
 
         # For `nix flake check`
         checks = {
+          # Build the crate as part of `nix flake check` for convenience
+          inherit ha-registry;
+
+          # Run clippy (and deny all warnings) on the crate source,
+          # again, reusing the dependency artifacts from above.
+          #
+          # Note that this is done as a separate derivation so that
+          # we can block the CI if there are issues here, but not
+          # prevent downstream consumers from building our crate by itself.
+          ha-registry-clippy = craneLib.cargoClippy (commonArgs
+            // {
+              inherit cargoArtifacts;
+              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+            });
+
+          ha-registry-doc = craneLib.cargoDoc (commonArgs
+            // {
+              inherit cargoArtifacts;
+            });
+
+          # Check formatting
+          ha-registry-fmt = craneLib.cargoFmt {
+            inherit src;
+          };
+
+          # Audit dependencies
+          ha-registry-audit = craneLib.cargoAudit {
+            inherit src advisory-db;
+          };
+
+          # Audit licenses
+          ha-registry-deny = craneLib.cargoDeny {
+            inherit src;
+          };
+
+          # Run tests with cargo-nextest
+          # Consider setting `doCheck = false` on `ha-registry` if you do not want
+          # the tests to run twice
+          ha-registry-nextest = craneLib.cargoNextest (commonArgs
+            // {
+              inherit cargoArtifacts;
+              partitions = 1;
+              partitionType = "count";
+            });
+
           status = pkgs.callPackage ./nixos/tests/status.nix {inherit packages;};
+
           pre-commit-check = let
             # some treefmt formatters are not supported in pre-commit-hooks we filter them out for now.
             toFilter = ["yamlfmt"];
@@ -217,13 +300,6 @@
                 };
             };
           formatting = treefmtEval.config.build.check self;
-          build = packages.check;
-          inherit
-            (packages)
-            default
-            test
-            ;
-          lint = packages.clippy;
         };
       }
     )
